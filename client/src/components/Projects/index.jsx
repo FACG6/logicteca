832e2122877349da4c6402f529084e58be302ce8
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-17 14:19:16 +0300

    create projects table with dummy data relates #17

--- client/src/components/Projects/index.jsx
+++ client/src/components/Projects/index.jsx
@@ -1,12 +1,73 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import ContentEditable from 'react-contenteditable';
+import './style.css';
+const data = require('../utils/projects');
 
-export default function Projects() {
-  return (
-    <div>
-      Projects
-      <Link to={'/project/1/1'}>project 1</Link>
-      <Link to={'/project/2/1'}>project 2</Link>
-    </div>
-  );
+class Projects extends Component {
+  state = {
+    data: data
+  };
+
+  render() {
+    const comlums = [
+      {
+        Header: 'Id',
+        accessor: 'id',
+        maxWidth: 80,
+        style: {
+          textAlign: 'center'
+        },
+        headerClassName: 'projects__table--header'
+      },
+      {
+        Header: 'Name',
+        accessor: 'name',
+        style: {
+          textAlign: 'center'
+        },
+        headerClassName: 'projects__table--header'
+      },
+      {
+        Header: 'Description',
+        accessor: 'description',
+        style: {
+          textAlign: 'center'
+        },
+        headerClassName: 'projects__table--header'
+      },
+      {
+        Header: 'Created At',
+        accessor: 'created_at',
+        style: {
+          textAlign: 'center'
+        },
+        headerClassName: 'projects__table--header',
+        Cell: rowInfo => {
+          return (
+            <div>
+              <span>{rowInfo.value}</span>
+              <span className="projects__table--edit">...</span>
+            </div>
+          );
+        }
+      }
+    ];
+    return (
+      <section className="projects__table">
+        <ReactTable
+          columns={comlums}
+          data={this.state.data}
+          filterable
+          showPagination={false}
+          defaultPageSize={this.state.data.length}
+          noDataText="....loading"
+        />
+      </section>
+    );
+  }
 }
+
+export default Projects;
---
+++ client/src/components/utils/projects.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    id: 1,
+    name: 'Demo',
+    description: 'this is the first project',
+    created_at: '13/10/2018'
+  },
+  {
+    id: 2,
+    name: 'Demo2',
+    description: 'this is the Second project',
+    created_at: '13/11/2018'
+  },
+  {
+    id: 3,
+    name: 'Demo3',
+    description: 'this is the Third project',
+    created_at: '13/12/2018'
+  }
+];
+
+module.exports = projects;
