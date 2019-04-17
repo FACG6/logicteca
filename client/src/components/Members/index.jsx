272a7dcaadbfb70b27ffa07e0d30a6b6419ee725
Israa <israa96sulaiman@gmail.com>
2019-04-18 02:04:25 +0300

    create users table Relates #19

--- client/src/components/Members/index.jsx
+++ client/src/components/Members/index.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Table, Icon, Dropdown, Menu } from 'antd';
 import Editable from 'react-contenteditable';
 import 'antd/dist/antd.css';
@@ -7,79 +7,169 @@ import './style.css';
 const users = require('../../utils/users.json');
 
 class Users extends Component {
-  state = {
-    users: [],
-    nameOptions: [],
-    fullNameOptions: [],
-    showSaveButton: false,
-    userNameError: null,
-    fullNameError: null,
-  }
+	state = {
+		users: [],
+		nameOptions: [],
+		fullNameOptions: [],
+		showSaveButton: false,
+		userNameError: null,
+		fullNameError: null,
+	};
 
-  componentDidMount(){
-  //Fetch to get users from database//
-    // fetch('/api/v1/members')
-    //   .then(response=>response.json())
-    //   .then(results=>results.data)
-    //   .then(**what is done bellow should be inside this then**)
-    
-  //Filtering the members to get unique names and fullnames for filtering//
-    const usernames = [];
-    const nameOptions = [];
-    const fullNames = [];
-    const fullNameOptions = [];
-    users.forEach(user => {
-      const username = user['user_name'];
-      const fullName = user['full_name'];
-      if (usernames.indexOf(username) === -1) {
-        usernames.push(username);
-        nameOptions.push({ text: username, value: username });
-      }
-      if(fullNames.indexOf(fullName) === -1){
-        fullNames.push(fullName);
-        fullNameOptions.push({ text: fullName, value: fullName});
-      }
-    });
+	componentDidMount() {
+		//Fetch to get users from database//
+		// fetch('/api/v1/members')
+		//   .then(response=>response.json())
+		//   .then(results=>results.data)
+		//   .then(**what is done bellow should be inside this then**)
 
-    //Store them in the state
-    this.setState({users, fullNameOptions, nameOptions});
-  }
+		//Filtering the members to get unique names and fullnames for filtering//
+		const usernames = [];
+		const nameOptions = [];
+		const fullNames = [];
+		const fullNameOptions = [];
+		users.forEach(user => {
+			const username = user['user_name'];
+			const fullName = user['full_name'];
+			if (usernames.indexOf(username) === -1) {
+				usernames.push(username);
+				nameOptions.push({ text: username, value: username });
+			}
+			if (fullNames.indexOf(fullName) === -1) {
+				fullNames.push(fullName);
+				fullNameOptions.push({ text: fullName, value: fullName });
+			}
+		});
 
-  handleAddUser = (event) => {
-    //Adding New Users
-  }
+		//Store them in the state
+		this.setState({ users, fullNameOptions, nameOptions });
+	}
 
-  handleEditUserInfo = () => {
-    //Editing UserInfo
-  }
+	handleAddUser = event => {
+		//Adding New Users
+	};
 
-  deleteUser = () => {
-    //Deleting
-  }
+	handleEditUserInfo = () => {
+		//Editing UserInfo
+	};
 
-  showSwal = () => {
-    //Swal before deleting
-  }
+	deleteUser = () => {
+		//Deleting
+	};
 
-  confirmDelete = () => {
-    //Confirm Delete
-  }
+	showSwal = () => {
+		//Swal before deleting
+	};
 
-  validateUserInfo = () => {
-    //Validation
-  }
+	confirmDelete = () => {
+		//Confirm Delete
+	};
 
-  updateUserInfo = () => {
+	validateUserInfo = () => {
+		//Validation
+	};
 
-  }
-
-  render(){
-    return (
-      <div>Members</div>
-
-    )
-  }
-
+	updateUserInfo = () => {};
+	render() {
+		//DropDown Menue//
+		const menu = (
+			<Menu>
+				<Menu.Item onClick={this.handleDeleteUser}>
+					<Icon type="user-delete" />
+					delete
+				</Menu.Item>
+				<Menu.Item>
+					<Icon type="lock" />
+					change password
+				</Menu.Item>
+			</Menu>
+		);
+		//Table Columns//
+		const columns = [
+			{
+				title: 'Username',
+				dataIndex: 'user_name',
+				render: (value, record) => {
+					return (
+						<Editable
+							innerRef={this.contentEditable}
+							html={value}
+							onChange={event => this.handleEditUserInfo(event, record, 'Username')}
+							tagName="span"
+							className="users__cell"
+						/>
+					);
+				},
+				filters: this.state.nameOptions,
+        onFilter: (value, record) => record['user_name'].indexOf(value) === 0,
+        defaultSort: 'descend',
+				sorter: (a, b) => {
+					if (a['user_name'] > b['user_name']) {
+						return -1;
+					}
+					if (a['user_name'] < b['user_name']) {
+						return 1;
+					}
+					return 0;
+				},
+			},
+			{
+				title: 'Full Name',
+				dataIndex: 'full_name',
+				render: (value, record) => {
+					return (
+						<Editable
+							html={value}
+							onChange={event => this.handleEdit(event, record, 'Full Name')}
+							tagName="span"
+							className="users__cell"
+						/>
+					);
+				},
+				defaultSort: 'descend',
+				sorter: (a, b) => {
+					if (a['full_name'] < b['full_name']) {
+						return -1;
+					}
+					if (a['full_name'] > b['full_name']) {
+						return 1;
+					}
+					return 0;
+				},
+			},
+			{
+				title: 'Role',
+				dataIndex: 'role',
+				render: (value, record) => {
+					return (
+						<select className="users__select">
+							<option value="developer">Developer</option>
+							<option value="scrum-master">Scrum Master</option>
+						</select>
+					);
+				},
+			},
+			{
+				render: props => {
+					return (
+						<Dropdown key={props.key} trigger={['click']} overlay={menu}>
+							<Icon title="click" className="user__ellipsis" type="ellipsis" />
+						</Dropdown>
+					);
+				},
+			},
+		];
+		return (
+			<Table
+				rowKey={record => record.id}
+				dataSource={this.state.users}
+				columns={columns}
+				pagination={false}
+				rowClassName="users__row"
+				className="users__table"
+			/>
+		);
+	}
 }
 
 export default Users;
