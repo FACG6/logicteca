f1e9916179e98b14388fe27173ca3e0bc0bccdfa
Israa <israa96sulaiman@gmail.com>
2019-04-19 11:42:54 +0300

    menu style file deleted

--- client/src/components/Users/menu/menu.jsx
+++ client/src/components/Users/menu/menu.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Menu, Icon } from 'antd';
 
-export default ({ handleDeleteUser, rowId, usersLength, handleAddPassword}) => {
-	return rowId === usersLength ?
-      (<Menu>
-        <Menu.Item onClick={handleAddPassword}>
-          <Icon type="lock" />
-          Add password
+export default ({ handleDeleteUser, rowId, usersLength, handleAddPassword }) => {
+	return rowId === usersLength ? (
+		<Menu>
+			<Menu.Item onClick={handleAddPassword}>
+				<Icon type="lock" />
+				Add password
 			</Menu.Item>
-      </Menu>):
-		(<Menu>
+		</Menu>
+	) : (
+		<Menu>
 			<Menu.Item onClick={handleDeleteUser}>
 				<Icon type="user-delete" />
 				Delete member
@@ -18,5 +19,6 @@ export default ({ handleDeleteUser, rowId, usersLength, handleAddPassword}) => {
 				<Icon type="lock" />
 				Change password
 			</Menu.Item>
-		</Menu>);
+		</Menu>
+	);
 };
