b0549b6a723dfeab53cc908686bbef0211e53d70
Israa <israa96sulaiman@gmail.com>
2019-04-19 18:42:10 +0300

    trying to remove spaces

--- client/src/components/Users/select/index.jsx
+++ client/src/components/Users/select/index.jsx
@@ -4,9 +4,7 @@ import './style.css'
 export default ({ defaultValue, onChange }) => {
 	return (
 		<select className="users__select" defaultValue={defaultValue} onChange={onChange}>
-			<option id="1" value="developer">
-				Developer
-			</option>
+			<option id="1" value="developer">Developer</option>
 			<option id="2" value="scrum master">
 				Scrum Master
 			</option>
