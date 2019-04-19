7bafdcce5cac79b884052c9cce63dc86e2931a0e
Israa <israa96sulaiman@gmail.com>
2019-04-19 18:43:55 +0300

    spaces issue with select compoennt

--- client/src/components/Users/select/index.jsx
+++ client/src/components/Users/select/index.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './style.css'
 
 export default ({ defaultValue, onChange }) => {
-	return (
-		<select className="users__select" defaultValue={defaultValue} onChange={onChange}>
-			<option id="1" value="developer">Developer</option>
-			<option id="2" value="scrum master">
-				Scrum Master
-			</option>
-		</select>
-	);
+  return (
+    <select className="users__select" defaultValue={defaultValue} onChange={onChange}>
+      <option id="1" value="developer">Developer</option>
+      <option id="2" value="scrum master">
+        Scrum Master
+      </option>
+    </select>
+  );
 };
