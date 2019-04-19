5c11e80cfbd375cd6e8fe4fe7f3ee422525210d8
Angham116 <angham.1161995@hotmail.com>
2019-04-19 17:23:59 +0300

    made project components as scrums

--- client/src/components/scrums/index.jsx
+++ client/src/components/scrums/index.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Tabs } from 'antd';
+import Scrum from "./scrum/index";
+const TabPane = Tabs.TabPane;
 
-export default class Scrum extends Component {
+export default class Scrums extends Component {
+  state = {
+    projectName: 'Abc1',
+    scrumsId: [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,]
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
-    const id = this.props.match.params.scrumId;
+    const { projectId } = this.props.match.params;
+    const { projectName, scrumsId } = this.state;
     return (
-      <>
-      <Link to={'/project/1/1'}>scrum 1</Link>
-      <Link to={'/project/1/2'}>scrum 2</Link>
-        <div>scrum {id}</div>
-      </>
+      <div>
+        <h2> {projectName} </h2>
+        <Tabs defaultActiveKey={scrumsId[0]}>
+          {this.state.scrumsId.map(item => {
+            return (
+                <TabPane tab={item} key={item} onClick={() => this.props.history.push(`/project/${projectId}/$     {item}`)}>
+                  <Scrum item={item}/>
+                </TabPane>
+            )
+          })}
+        </Tabs>
+      </div>
     );
   }
 }
