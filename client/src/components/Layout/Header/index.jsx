a8cfcb4d8ec3432e567ca4fd14f73a0125091757
aminalakhsham <aminking140@gmail.com>
2019-04-19 12:06:39 +0300

    remove changes in other files not relates to PR and fix filter

--- client/src/components/Layout/Header/index.jsx
+++ client/src/components/Layout/Header/index.jsx
@@ -1,46 +1,34 @@
-import React, { Component } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./style.css";
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './style.css';
 
 class Header extends Component {
   state = {
     userList: false
-  };
+  }
 
-  handleUserList = e => {
-    this.setState({ userList: !this.state.userList });
-  };
+  handleUserList = (e) => {
+    this.setState({userList: !this.state.userList})
+  }
 
-  render() {
+   render() { 
     return (
-      <div className="header">
-        <img
-          className="header__logo"
-          src="http://logicteca.com/wp-content/uploads/2015/11/logicteca-logo.png"
-        />
-        <div className="header__right">
-          <ul className="navbar">
-            <li> Projects </li>
-            <li> Members </li>
-          </ul>
-          <div className="header__userList">
-            <button className="header__btn" onClick={this.handleUserList}>
-              {" "}
-              Username
-              <FontAwesomeIcon className="search__icon" icon="caret-down" />
-            </button>
-            <a
-              className={`header__logout ${
-                this.state.userList ? "header__btn--hidden" : ""
-              }`}
-            >
-              {" "}
-              Logout{" "}
-            </a>
+      <div className='header'>
+          <img className='header__logo' src='http://logicteca.com/wp-content/uploads/2015/11/logicteca-logo.png'/>
+          <div className='header__right'>
+              <ul className='navbar'>
+                  <li> Projects </li>
+                  <li> Members </li> 
+              </ul>
+              <div className='header__userList'>
+              <button className='header__btn' onClick={this.handleUserList}> Username 
+                  <FontAwesomeIcon className='search__icon' icon='caret-down' />
+              </button>
+                <a className={`header__logout ${this.state.userList ? 'header__btn--hidden' : ''}`}> Logout </a>  
+              </div>
           </div>
-        </div>
       </div>
-    );
+    )
   }
 }
 
--- client/src/components/Projects/ProjectNew/TableMember/TableMember.jsx
+++ client/src/components/Projects/ProjectNew/TableMember/TableMember.jsx
@@ -4,16 +4,8 @@ import "./style.css";
 import filterData from "./logic";
 
 export default class TableMember extends Component {
-  constructor(props) {
-    super(props);
-    this.state.member = require("./member.json");
-    this.state.filterArray = filterData(this.state.member);
-    this.columns[0].filters = this.state.filterArray[1];
-    this.columns[1].filters = this.state.filterArray[2];
-  }
-
   state = {
-    member: [],
+    member: require("./member.json"),
     filterArray: [],
     pagination: false,
     rowSelection: {
@@ -25,44 +17,47 @@ export default class TableMember extends Component {
     },
     scroll: { y: 400 }
   };
-
-  columns = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-      width: 150,
-      filters: this.state.filterArray[1],
-      onFilter: (value, record) => record.name.indexOf(value) === 0,
-      sorter: (a, b) => {
-        if (a.id > b.id) {
-          return -1;
-        }
-        if (a.id < b.id) {
-          return 1;
-        }
-        return 0;
-      }
-    },
-    {
-      title: "role",
-      dataIndex: "role",
-      key: "role",
-      width: 150,
-      filters: this.state.filterArray[2],
-      onFilter: (value, record) => record.role.indexOf(value) === 0
-    }
-  ];
+  componentDidMount() {
+    this.setState({ filterArray: filterData(this.state.member) });
+  }
 
   render() {
+    const columns = [
+      {
+        title: "Name",
+        dataIndex: "name",
+        key: "name",
+        width: 150,
+        filters: this.state.filterArray[1],
+        onFilter: (value, record) => record.name.indexOf(value) === 0,
+        sorter: (a, b) => {
+          if (a.id > b.id) {
+            return -1;
+          }
+          if (a.id < b.id) {
+            return 1;
+          }
+          return 0;
+        }
+      },
+      {
+        title: "role",
+        dataIndex: "role",
+        key: "role",
+        width: 150,
+        filters: this.state.filterArray[2],
+        onFilter: (value, record) => record.role.indexOf(value) === 0
+      }
+    ];
     const { pagination, rowSelection, scroll } = this.state;
+    console.log(this.state.filterArray);
     return (
       <Table
         rowKey={record => record.id}
         pagination={pagination}
         rowSelection={rowSelection}
         scroll={scroll}
-        columns={this.columns}
+        columns={columns}
         dataSource={this.state.member}
         rowClassName="table-font-row"
         className="table-font-header"
