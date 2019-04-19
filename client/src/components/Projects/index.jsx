9d69627d852c4685eaf34aa64ced15264b076afa
Angham116 <angham.1161995@hotmail.com>
2019-04-19 17:22:42 +0300

    made project components as scrums

--- client/src/components/Projects/index.jsx
+++ client/src/components/Projects/index.jsx
@@ -5,8 +5,8 @@ export default function Projects() {
   return (
     <div>
       Projects
-      <Link to={'/project/1/1'}>project 1</Link>
-      <Link to={'/project/2/1'}>project 2</Link>
+      <Link to={'/project/1'}>project 1</Link>
+      <Link to={'/project/2'}>project 2</Link>
     </div>
   );
 }
