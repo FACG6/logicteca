d17a9f3fcfeb4ecf9ea15362bfdd50cbd7d053e0
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-19 07:44:39 +0300

    remove redirect from import, no need to import it relates #17

--- client/src/components/Projects/projectsTable/index.jsx
+++ client/src/components/Projects/projectsTable/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Table, Dropdown, Icon, Menu } from "antd";
 import Swal from "sweetalert2";
 import "antd/dist/antd.css";
