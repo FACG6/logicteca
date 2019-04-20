6f8a31decdd3801028b18bcd042395b87180b794
Israa <israa96sulaiman@gmail.com>
2019-04-20 15:02:22 +0300

    create handleChange and submit functions Relates #16

--- client/src/components/Login/loginForm/index.jsx
+++ client/src/components/Login/loginForm/index.jsx
@@ -8,20 +8,31 @@ export default class Login extends Component {
     username: '',
     password: '',
   }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  }
+
+  login = (event) => {
+    event.preventDefault();
+    const { password, username } = this.state;
+    //fetch//
+  }
+
   render() {
     return (
       <div className='login__form__container'>
         <img className='login__image' src={image} alt='user' />
-        <form className='login__form'>
+        <form onSubmit={this.login} className='login__form'>
           <div className='login__input-container'>
             <Icon className='login__form__icon' type="user" />
-            <input placeholder='Username' className='login__form__input' id='username' name='username' type='text' value={this.state.username}></input>
+            <input onChange={this.handleChange} placeholder='Username' className='login__form__input' id='username' name='username' type='text' value={this.state.username}></input>
           </div>
           <div className='login__input-container'>
             <Icon className='login__form__icon' type="lock" />
-            <input placeholder='Password' className='login__form__input' id='password' name='password' type='password' value={this.state.password}></input>
+            <input onChange={this.handleChange} placeholder='Password' className='login__form__input' id='password' name='password' type='password' value={this.state.password}></input>
           </div>
-          <input  className='login__form__submit' id='login' type='submit' value='Login'></input>
+          <input className='login__form__submit' id='login' type='submit' value='Login'></input>
         </form >
       </div >
     )
