a90ef89a6de966a4c0c8be35a13d4b6a419a2a06
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-20 06:46:27 +0300

    move columns array to the class body except the last inner object element relates #17

--- client/src/components/Projects/projectsTable/index.jsx
+++ client/src/components/Projects/projectsTable/index.jsx
@@ -54,6 +54,61 @@ class ProjectsTable extends Component {
       }
     });
   };
+  columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      defaultSort: "descend",
+      sorter: (a, b) => {
+        if (a.id > b.id) {
+          return -1;
+        }
+        if (a.id < b.id) {
+          return 1;
+        }
+        return 0;
+      }
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      defaultSort: "descend",
+      sorter: (a, b) => {
+        if (a["name"] > b["name"]) {
+          return -1;
+        }
+        if (a["name"] < b["name"]) {
+          return 1;
+        }
+        return 0;
+      },
+      render: (text, record) => {
+        return (
+          <Link to={`/project/${record.id}/1`}>
+            <span className="projects__table--row-link">{text}</span>
+          </Link>
+        );
+      }
+    },
+    {
+      title: "Description",
+      dataIndex: "description"
+    },
+    {
+      title: "Created At",
+      dataIndex: "created_at",
+      defaultSort: "descend",
+      sorter: (a, b) => {
+        if (a["created_at"] > b["created_at"]) {
+          return -1;
+        }
+        if (a["created_at"] < b["created_at"]) {
+          return 1;
+        }
+        return 0;
+      }
+    }
+  ];
   render() {
     const { data } = this.state;
     if (this.state.redirect) {
@@ -75,80 +130,25 @@ class ProjectsTable extends Component {
         </Menu.Item>
       </Menu>
     );
-    const columns = [
-      {
-        title: "ID",
-        dataIndex: "id",
-        defaultSort: "descend",
-        sorter: (a, b) => {
-          if (a.id > b.id) {
-            return -1;
-          }
-          if (a.id < b.id) {
-            return 1;
-          }
-          return 0;
-        }
-      },
-      {
-        title: "Name",
-        dataIndex: "name",
-        defaultSort: "descend",
-        sorter: (a, b) => {
-          if (a["name"] > b["name"]) {
-            return -1;
-          }
-          if (a["name"] < b["name"]) {
-            return 1;
-          }
-          return 0;
-        },
-        render: (text, record) => {
-          return (
-            <Link to={`/project/${record.id}/1`}>
-              <span className="projects__table--row-link">{text}</span>
-            </Link>
-          );
-        }
-      },
-      {
-        title: "Description",
-        dataIndex: "description"
-      },
-      {
-        title: "Created At",
-        dataIndex: "created_at",
-        defaultSort: "descend",
-        sorter: (a, b) => {
-          if (a["created_at"] > b["created_at"]) {
-            return -1;
-          }
-          if (a["created_at"] < b["created_at"]) {
-            return 1;
-          }
-          return 0;
-        }
-      },
-      {
-        render: props => {
-          return (
-            <Dropdown key={props.id} trigger={["click"]} overlay={menu}>
-              <Icon
-                onMouseOver={() => this.handleRow(props.id)}
-                title="click"
-                className="projects__table--edit"
-                type="ellipsis"
-              />
-            </Dropdown>
-          );
-        }
+    this.columns[4] = {
+      render: props => {
+        return (
+          <Dropdown key={props.id} trigger={["click"]} overlay={menu}>
+            <Icon
+              onMouseOver={() => this.handleRow(props.id)}
+              title="click"
+              className="projects__table--edit"
+              type="ellipsis"
+            />
+          </Dropdown>
+        );
       }
-    ];
+    };
     return (
       <section className="projects__table">
         <Table
           dataSource={data}
-          columns={columns}
+          columns={this.columns}
           pagination={false}
           rowKey={record => record.id}
           rowClassName="projects__row"
