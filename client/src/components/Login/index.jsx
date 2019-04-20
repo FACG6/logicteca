6487184791168552203ba2df0e9390e23a41c769
Israa <israa96sulaiman@gmail.com>
2019-04-20 13:55:32 +0300

    create login component and its css Relates #16

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {Component} from 'react';
+import About from './about/index';
+import LoginFrom from './loginForm/index';
+import './style.css'
 
-export default function Login() {
-  return (
-    <div>
-      Login page
-    </div>
-  )
+export default class Login extends Component{
+  state = {
+    username: '',
+    password: '',
+  }
+  render(){
+    return (
+      <div className='login__container'>
+        <LoginFrom />
+        <About />
+      </div>
+    )
+  }
 }
+
+
