01329fb2ae1158d1f1709316cf30e730d53a0d19
Israa <israa96sulaiman@gmail.com>
2019-04-20 13:53:22 +0300

    create about component and its css Relates #16

---
+++ client/src/components/Login/about/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css'
+
+export default function (){
+  return (
+    <div className='login__about-section'>
+    <div className='login__about'>
+      <h1 className='login__about__heading'>Logicteca Scrummer</h1>
+      <p className='login__about__content'>The new age of outsourced Software Development, leading outsourcing solution that most organizations have been looking for.</p>
+      </div>
+    </div>
+  )
+
+}
