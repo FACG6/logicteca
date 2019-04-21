bf6dce8dc7c7e7a609a51a7feca62641ce8da98b
Israa <israa96sulaiman@gmail.com>
2019-04-21 09:53:34 +0300

    change class names and edit input tag Relates #16

--- client/src/components/Login/loginForm/index.jsx
+++ client/src/components/Login/loginForm/index.jsx
@@ -25,24 +25,24 @@ export default class Login extends Component {
 
   render() {
     return (
-      <div className='login__form__container'>
+      <section className='login__form-container'>
         <img className='login__image' src={image} alt='user' />
         <form onSubmit={this.login} className='login__form'>
           <div className='login__input-container'>
-            <Icon className='login__form__icon' type="user" />
-            <input onChange={this.handleChange} placeholder='Username' className='login__form__input' id='username' name='username' type='text' value={this.state.username}></input>
+            <Icon className='login__icon' type="user" />
+            <input onChange={this.handleChange} placeholder='Username' className='login__input' id='username' name='username' type='text' value={this.state.username}/>
           </div>
           <div className='login__input-container'>
-            <Icon className='login__form__icon' type="lock" />
-            <input onChange={this.handleChange} placeholder='Password' className='login__form__input' id='password' name='password' type='password' value={this.state.password}></input>
+            <Icon className='login__icon' type="lock" />
+            <input onChange={this.handleChange} placeholder='Password' className='login__input' id='password' name='password' type='password' value={this.state.password}/>
           </div>
           {this.state.loginError ? (
             <span className='login__error'>Fill All Fields</span>
           )
             : null}
-          <input className='login__form__submit' id='login' type='submit' value='Login'></input>
+          <input className='login__submit-btn' id='login' type='submit' value='Login'/>
         </form >
-      </div >
+      </section>
     )
   }
 }
