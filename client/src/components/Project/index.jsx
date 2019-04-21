554d54c3ae2cb26a251f5d69b2e464deab738e74
Angham116 <angham.1161995@hotmail.com>
2019-04-21 13:16:48 +0300

    edit

--- client/src/components/Project/index.jsx
+++ client/src/components/Project/index.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { Button } from "antd";
 import Scrum from "./Scrums/index";
-import './style.css'
+import './style.css';
+const scrums = require('./Scrums/utilis/scrums.json')
 
 class Scrums extends Component {
   state = {
@@ -13,56 +14,6 @@ class Scrums extends Component {
   componentDidMount() {
     // fetch
     const projectName = 'Demo';
-    const scrums = [
-      {
-        id: '1',
-        scrumName: 'scrum 1'
-      },
-      {
-        id: '2',
-        scrumName: 'scrum 2'
-      },
-      {
-        id: '3',
-        scrumName: 'scrum 3'
-      },
-      {
-        id: '4',
-        scrumName: 'scrum 4'
-      },
-      {
-        id: '5',
-        scrumName: 'scrum 5'
-      },
-      {
-        id: '6',
-        scrumName: 'scrum 6'
-      },
-      {
-        id: '7',
-        scrumName: 'scrum 7'
-      },
-      {
-        id: '8',
-        scrumName: 'scrum 8'
-      },
-      {
-        id: '9',
-        scrumName: 'scrum 9'
-      },
-      {
-        id: '10',
-        scrumName: 'scrum 10'
-      },
-      {
-        id: '11',
-        scrumName: 'scrum 11'
-      },
-      {
-        id: '12',
-        scrumName: 'scrum 12'
-      }
-    ];
     this.setState({ scrums: scrums });
     this.setState({projectName: projectName})
   }
