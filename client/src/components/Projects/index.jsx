0a8a520cb4a27458be556eac4e521bb7ae3ba192
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-22 11:25:29 +0300

    change the color of delete confirmation relates #17

--- client/src/components/Projects/index.jsx
+++ client/src/components/Projects/index.jsx
@@ -44,8 +44,8 @@ class Projects extends Component {
       text: "You won't be able to revert this!",
       type: "warning",
       showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
       confirmButtonText: `Yes, ! delete "${name}"`
     }).then(result => {
       if (result.value) {
