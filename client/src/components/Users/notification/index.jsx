03c51ef476da4acc90ce870dd625f50cadacf502
Israa <israa96sulaiman@gmail.com>
2019-04-23 11:20:45 +0300

    fix bug in displaying and hiding password button, remove validation for editing and add a new notificatio message Relates #19

--- client/src/components/Users/notification/index.jsx
+++ client/src/components/Users/notification/index.jsx
@@ -5,10 +5,13 @@ import {
 const createNotification = (type) => {
   switch (type) {
     case 'password':
-      NotificationManager.success('Password added successfully. Save all Changes Now', null, 5000);
+      NotificationManager.success('Password added successfully. Save all Changes Now', null, 1000);
       break;
     case 'success':
-      NotificationManager.success('Added successfully', null, 0);
+      NotificationManager.success('Added successfully', null, 5000);
+      break;
+    case 'row exist':
+      NotificationManager.error('Please, save the previous member before adding a new one!', null, 5000);
       break;
     case 'error':
       NotificationManager.error('Oops', 'Something went wrong!')
