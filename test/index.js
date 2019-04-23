d368f5563b1fd69ae8bcd7c7891bebc5f2e9e229
aminalakhsham <aminking140@gmail.com>
2019-04-24 00:11:40 +0300

    fix index file to read all file of test in folders test

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-shadow */
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
 const testFolder = './test';
@@ -9,8 +10,17 @@ fs.readdir(testFolder, (err, files) => {
   } else {
     files.forEach((file) => {
       if (file !== 'index.js') {
-        require(`./${file}/test`);
+        fs.readdir(`${testFolder}/${file}`, (err, files) => {
+          if (err) {
+            console.log(err);
+          } else {
+            files.forEach((subFile) => {
+              require(`./${file}/${subFile}`);
+            });
+          }
+        });
       }
     });
   }
 });
+// require('./server/projectsQuery.test');
