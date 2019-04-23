4b305a66b550ac6962ea9f94b5347bb659c61b16
Israa <israa96sulaiman@gmail.com>
2019-04-23 22:06:27 +0300

    add a select component for assignee Relates #25

--- client/src/components/Project/Scrums/select/index.jsx
+++ client/src/components/Project/Scrums/select/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function StatusSelect ({ defaultValue, onChange }) {
+function StatusSelect({ defaultValue, onChange }) {
   return (
     <select className="status__select" defaultValue={defaultValue} onChange={onChange}>
       <option id="1" value="completed">Completed</option>
@@ -12,15 +12,23 @@ function StatusSelect ({ defaultValue, onChange }) {
   );
 };
 
-function ActionTypeSelect ({ defaultValue, onChange }) {
-    return (
-      <select className="actionType__select" defaultValue={defaultValue} onChange={onChange}>
-        <option id="1" value="coding">Coding</option>
-        <option id="2" value="testing">Testing</option>
-        <option id="3" value="changes">Changes</option>
-        <option id="4" value="fixBugs">Fix Bugs</option>
-      </select>
-    );
-  };
+function ActionTypeSelect({ team, defaultValue, onChange }) {
+  return (
+    <select className="actionType__select" defaultValue={defaultValue} onChange={onChange}>
+      <option id="1" value="coding">Coding</option>
+      <option id="2" value="testing">Testing</option>
+      <option id="3" value="changes">Changes</option>
+      <option id="4" value="fixBugs">Fix Bugs</option>
+    </select>
+  );
+};
 
-export {StatusSelect, ActionTypeSelect}
+function ProjectTeam({team, onChange, defaultValue}) {
+  return (
+    <select className="projectTeam__select" defaultValue={defaultValue} onChange={onChange}>
+      {team.map(member => <option key={member.id} id={member.id} value={member.name}>{member.name}</option>)}
+    </select>
+  );
+}
+
+export { StatusSelect, ActionTypeSelect, ProjectTeam }
