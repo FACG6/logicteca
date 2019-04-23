9301fb5057cb253f043c00a675662fde3c5c85be
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-23 20:47:35 +0300

    modify server/test to test insertUser query relates # 107

--- test/server/test.js
+++ test/server/test.js
@@ -1,6 +1,16 @@
 const test = require('tape');
+const buildDB = require('../../server/database/config/dbBuild');
+const insertUser = require('../../server/database/queries/insertUser');
 
-test('test', (t) => {
-  t.equal(1, 1, 'done');
-  t.end();
+// Database Queries Tests
+test('test insertUser database Query', (t) => {
+  buildDB()
+    .then(() => insertUser('Aymanco', 'Ayman AlQoqa', 'developper', '1234567'))
+    .then((result) => {
+      t.equal(result.rowCount, 1, 'New user is inserted');
+      t.equal(Object.keys(result.rows[0]).length, 5, 'All users name Fields inserted Successfully');
+      t.equal(result.rows[0].user_name, 'Aymanco', 'Username is inserted successfully');
+      t.end();
+    })
+    .catch(err => t.error(err));
 });
