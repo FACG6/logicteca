ebe6169972adad853b7dd3b86bf1675551440311
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-23 20:55:22 +0300

    modify server/test to test getUsers query relates # 107

--- test/server/test.js
+++ test/server/test.js
@@ -1,6 +1,7 @@
 const test = require('tape');
 const buildDB = require('../../server/database/config/dbBuild');
 const insertUser = require('../../server/database/queries/insertUser');
+const getUsers = require('../../server/database/queries/getUsers');
 
 // Database Queries Tests
 test('test insertUser database Query', (t) => {
@@ -10,6 +11,19 @@ test('test insertUser database Query', (t) => {
       t.equal(result.rowCount, 1, 'New user is inserted');
       t.equal(Object.keys(result.rows[0]).length, 5, 'All users name Fields inserted Successfully');
       t.equal(result.rows[0].user_name, 'Aymanco', 'Username is inserted successfully');
+      t.end();
+    })
+    .catch(err => t.error(err));
+});
+test('test getUsers database Query', (t) => {
+  getUsers()
+    .then((result) => {
+      t.equal(typeof result.rows && typeof result.rows[0], 'object', 'return object');
+      t.equal(
+        Object.keys(result.rows[0]).length,
+        5,
+        'All users name Fields retreived Successfully',
+      );
       t.end();
     })
     .catch(err => t.error(err));
