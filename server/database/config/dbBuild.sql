70d4d8dc4c084b535e6cc4f71738e8cf02f22d7a
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-23 14:26:15 +0300

    use uuid in all ids

--- server/database/config/dbBuild.sql
+++ server/database/config/dbBuild.sql
@@ -5,11 +5,12 @@ DROP TYPE IF EXISTS act_type;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users(
-    id SERIAL  PRIMARY KEY,
+   id uuid DEFAULT uuid_generate_v4(),
     user_name VARCHAR NOT NULL,
     full_name VARCHAR,
     password TEXT NOT NULL,
-    role TEXT
+    role TEXT,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE projects(
@@ -21,21 +22,23 @@ CREATE TABLE projects(
 );
 
 CREATE TABLE users_projects(
-    id SERIAL PRIMARY KEY,
+    id uuid DEFAULT uuid_generate_v4(),
     project_id uuid REFERENCES projects(id),
-    user_id INTEGER REFERENCES users(id)
+    user_id uuid REFERENCES users(id),
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE scrums(
-    id SERIAL PRIMARY KEY,
+    id uuid DEFAULT uuid_generate_v4(),
     name VARCHAR NOT NULL, 
-    project_id uuid REFERENCES projects(id)
+    project_id uuid REFERENCES projects(id),
+    PRIMARY KEY(id)
 );
 
 CREATE TYPE act_type AS ENUM ('DEVELOPPER, SCRUM MASTER', 'CLIENT');
 
 CREATE TABLE tasks(
-    id SERIAL PRIMARY KEY,
+    id uuid DEFAULT uuid_generate_v4(),
     action_type act_type,
     status VARCHAR,
     modules VARCHAR,
@@ -49,8 +52,9 @@ CREATE TABLE tasks(
     total_efforts float,
     date_to_commit date,
     review_and_test_note TEXT, 
-    scrum_id INTEGER REFERENCES scrums(id),
-    assigned_to INTEGER REFERENCES users(id)
+    scrum_id uuid REFERENCES scrums(id),
+    assigned_to uuid REFERENCES users(id),
+    PRIMARY KEY(id)
 );
 
 
