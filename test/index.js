3eb2bf09f78f403c362196aa6c2d021387d1e9d2
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-24 10:55:09 +0300

    modify test/index to require selectUsers test file relates #105

--- test/index.js
+++ test/index.js
@@ -1,16 +1,3 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
-const testFolder = './test';
-const fs = require('fs');
-
-fs.readdir(testFolder, (err, files) => {
-  if (err) {
-    console.log(err);
-  } else {
-    files.forEach((file) => {
-      if (file !== 'index.js') {
-        require(`./${file}/test`);
-      }
-    });
-  }
-});
+require('./server/selectUsers');
