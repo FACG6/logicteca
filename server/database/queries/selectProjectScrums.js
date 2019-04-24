6fa518d15d00f77154a5d0ea420db93b9fe14744
Angham116 <angham.1161995@hotmail.com>
2019-04-24 14:19:09 +0300

    delete project description from query

--- server/database/queries/selectProjectScrums.js
+++ server/database/queries/selectProjectScrums.js
@@ -2,7 +2,7 @@ const connect = require('./../config/connection');
 
 const selectProjectScrums = (id) => {
   const getScrums = {
-    text: 'SELECT projects.id as project_id, projects.name, projects.description, scrums.id as scrum_id, scrums.name  FROM scrums inner join projects ON projects.id = scrums.project_id where projects.id = $1',
+    text: 'SELECT projects.id as project_id, projects.name, scrums.id as scrum_id, scrums.name  FROM scrums inner join projects ON projects.id = scrums.project_id where projects.id = $1',
     values: [id],
   };
   return connect.query(getScrums);
