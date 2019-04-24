7f8ba89ab266abb2bd303084872321605ee98dee
Angham116 <angham.1161995@hotmail.com>
2019-04-24 14:20:16 +0300

    edit the test for testing the keys of returnung object from db

--- test/server/projectScrums.js
+++ test/server/projectScrums.js
@@ -2,15 +2,9 @@ const test = require('tape');
 const { selectProjectScrums } = require('../../server/database/queries/selectProjectScrums');
 
 test('Testing query for select project scrums of valid project id', (t) => {
-  const scrum = {
-    project_id: '7d2b3be6-e58a-4591-94fc-f76e17d87aea',
-    name: 'Scrum1',
-    description: 'Custom built task management system',
-    scrum_id: '1d675bb9-eaf2-4a11-8f29-c17e71af337e',
-  };
   selectProjectScrums('7d2b3be6-e58a-4591-94fc-f76e17d87aea')
     .then((res) => {
-      t.deepEqual(res.rows[0], scrum, 'Should return row of scrum of project');
+      t.deepEqual(Object.keys(res.rows[0]), ['project_id', 'name', 'scrum_id'], 'Should return all obj keys for scrum of project');
       t.end();
     })
     .catch((err) => {
