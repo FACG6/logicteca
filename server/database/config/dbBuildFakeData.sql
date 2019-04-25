e13667c951b030fba7f4f49623f0e5dac74c748b
Israa <israa96sulaiman@gmail.com>
2019-04-25 16:15:20 +0300

    add BEGIN and COMMIT to SQL Relates #133

--- server/database/config/dbBuildFakeData.sql
+++ server/database/config/dbBuildFakeData.sql
@@ -1,3 +1,5 @@
+BEGIN;
+
 INSERT INTO users (user_name,full_name,password,role) VALUES ('amin','Amin ALakhsham','123','Developer');
 INSERT INTO users (user_name,full_name,password,role) VALUES ('israa','Israa Suliman','123','Scrum');
 INSERT INTO users (user_name,full_name,password,role) VALUES ('ahmed','Ahmed Shatat','123','Developer');
@@ -5,3 +7,5 @@ INSERT INTO users (user_name,full_name,password,role) VALUES ('ahmed','Ahmed Sha
 INSERT INTO projects (name,description) VALUES ('logicteca','Custom built task management system');
 INSERT INTO projects (name,description) VALUES ('AMIDEAST','connecting students with our different programs has been a challenge');
 INSERT INTO projects (name) VALUES ('Digital Cloud');
+
+COMMIT;
