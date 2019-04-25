df21f6be9ca7b4aabb8e3c2d7ec30ca796b72ac0
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-25 14:19:24 +0300

    resolve wrong files updates

--- server/controllers/projects/getProject.js
+++ server/controllers/projects/getProject.js
@@ -1,16 +1,3 @@
-const selectProjectDetails = require('../../database/queries/selectProjectDetails');
-
-exports.getProject = (req, res, next) => {
-  const { projectId } = req.params;
-  selectProjectDetails(projectId)
-    .then((result) => {
-      console.log(result);
-
-      const projectsData = {
-        data: result,
-        error: null,
-      };
-      return res.send(projectsData);
-    })
-    .catch(e => next(e));
+exports.getProject = (req, res) => {
+  res.send('single project page');
 };
--- test/index.js
+++ test/index.js
@@ -3,4 +3,3 @@ require('./server/projectsQuery.test');
 require('./server/projectDetailsQuery.test');
 require('./server/scrumTasks.test');
 require('./server/projectScrums');
-require('./server/getProjectRoute');
