24b368c37af1d7dc03eabd536ba6e0f3a00b22c5
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-25 12:08:00 +0300

    add getProjectsRoute test file relates #54

---
+++ test/server/getProjectsRoute.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+const test = require('tape');
+const app = require('../../server/app');
+
+test('Request projects route', (t) => {
+  request(app)
+    .get('/api/v1/projects')
+    .expect(200)
+    .expect('Content-Type', 'application/json')
+    .end((err, res) => {
+      if (err) {
+        t.error(err);
+      } else {
+        //
+        t.end();
+      }
+    });
+});
