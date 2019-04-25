c8f39c3287d6578b5a09024c67ad9958b5c09be8
Angham116 <angham.1161995@hotmail.com>
2019-04-25 15:59:04 +0300

    import getScrumRoute test file #55

---
+++ test/server/getScrumRoute.js
@@ -0,0 +1,40 @@
+const test = require('tape');
+const supertest = require('supertest');
+const route = require('../../server/app');
+const buildDB = require('../../server/database/config/dbBuild');
+const connect = require('../../server/database/config/connection');
+
+buildDB();
+  // .then( () => console.log(true))
+  // .catch( err => console.log(err))
+
+const selectScrum = () => {
+  const getScrums = {
+    text: 'SELECT * from scrums',
+  };
+  return connect.query(getScrums);
+};
+
+test('Testing for api/v1/scrums/:scrumId route', (t) => {
+  selectScrum()
+    .then((result) => {
+      console.log(9999999, result);
+      supertest(route)
+        .get(`api/v1/scrums/${result.rows[0].id}`)
+        // .get('api/v1/scrums/:scrumId')
+        .expect(200)
+        .expect('content-type', /html/)
+        .end((err, res) => {
+          if (err) {
+            console.log(8888, err);
+            t.error(err);
+            t.end();
+          } else {
+            console.log(9999999, typeof res.body);
+            t.equal(typeof res.body, 'Object', 'Shoud return type of the body is object');
+            t.end();
+          }
+        });
+    })
+    .catch(err => console.log(555, err));
+});
