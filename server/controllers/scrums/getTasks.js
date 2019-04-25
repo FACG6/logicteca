b5b6d521b933904bce7562013f4ddd980c6104e6
Angham116 <angham.1161995@hotmail.com>
2019-04-25 16:57:34 +0300

    get scrumId from request #55

--- server/controllers/scrums/getTasks.js
+++ server/controllers/scrums/getTasks.js
@@ -1,1 +1,10 @@
-exports.getTasks = (req, res) => res.send('single Tasks get');
+const selectScrumTasks = require('../../database/queries/selectScrumTasks');
+
+exports.getTasks = (req, res) => {
+  console.log(222, req.params.scrumId);
+  // console.log(33333333);
+  selectScrumTasks(req.params.scrumId)
+    .then(result => console.log(result.rwos[0]))
+    .catch(err => console.log(err));
+  res.send('single Tasks get');
+};
