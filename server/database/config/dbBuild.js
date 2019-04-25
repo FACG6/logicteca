eb209a217c1eed7f443172b3209e557fee0ed5d1
Israa <israa96sulaiman@gmail.com>
2019-04-25 16:01:39 +0300

    remove NODE_ENV condition Relates #133

--- server/database/config/dbBuild.js
+++ server/database/config/dbBuild.js
@@ -2,14 +2,8 @@ const { readFileSync } = require('fs');
 const path = require('path');
 const connect = require('./connection');
 
-let sql = '';
-if (process.env.NODE_ENV === 'test') {
-  sql = readFileSync(path.join(__dirname, 'dbBuildFakeData.sql')).toString();
-} else {
-  sql = readFileSync(path.join(__dirname, 'dbBuild.sql')).toString();
-}
+const sql = readFileSync(path.join(__dirname, 'dbBuild.sql')).toString();
 
 const buildDB = () => connect.query(sql);
-
 
 module.exports = buildDB;
