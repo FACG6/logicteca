f16969976eef8038a398c93ab2d26957feae3a86
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-25 13:50:13 +0300

    modify getProject relates #58

--- server/controllers/projects/getProject.js
+++ server/controllers/projects/getProject.js
@@ -1,1 +1,16 @@
-exports.getProject = (req, res) => res.send('single project get');
+const selectProjectDetails = require('../../database/queries/selectProjectDetails');
+
+exports.getProject = (req, res, next) => {
+  const { projectId } = req.params;
+  selectProjectDetails(projectId)
+    .then((result) => {
+      console.log(result);
+
+      const projectsData = {
+        data: result,
+        error: null,
+      };
+      return res.send(projectsData);
+    })
+    .catch(e => next(e));
+};
