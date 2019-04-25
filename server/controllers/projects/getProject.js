6749cb179e5a9c6544f445c89b1e3af966270dd2
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-25 16:49:40 +0300

    fix return data relates #58

--- server/controllers/projects/getProject.js
+++ server/controllers/projects/getProject.js
@@ -4,10 +4,8 @@ exports.getProject = (req, res, next) => {
   const { projectId } = req.params;
   selectProjectDetails(projectId)
     .then((result) => {
-      console.log(result);
-
       const projectsData = {
-        data: result,
+        data: result.rows,
         error: null,
       };
       return res.send(projectsData);
