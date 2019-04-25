430e3597050116573d2e4885d58ca28e8df6251b
Israa <israa96sulaiman@gmail.com>
2019-04-25 04:29:19 +0300

    edit deleteUser handler function Relates #52

--- server/controllers/users/deleteUser.js
+++ server/controllers/users/deleteUser.js
@@ -1,1 +1,14 @@
-exports.deleteUser = (req, res) => res.send('single user delete');
+const deleteUser = require('../../database/queries/deleteUser');
+
+exports.deleteUser = (req, res) => {
+  const { userId } = req.params;
+  deleteUser(userId)
+    .then((results) => {
+      if (results.rowCount) {
+        res.send({ error: null, data: results.rows[0].id });
+      } else {
+        res.status(401).send({ error: { status: 401, msg: 'No user available with this id' } });
+      }
+    })
+    .catch(() => res.send({ error: { code: 500, msg: 'Internal Server Error' }, data: null }));
+};
