237acf70f794d49d72cd16199c88bc264cbcbd6f
Angham116 <angham.1161995@hotmail.com>
2019-04-26 12:30:31 +0300

    getTasks of scrum route 55

--- server/controllers/scrums/getTasks.js
+++ server/controllers/scrums/getTasks.js
@@ -1,10 +1,25 @@
 const selectScrumTasks = require('../../database/queries/selectScrumTasks');
 
 exports.getTasks = (req, res) => {
-  console.log(222, req.params.scrumId);
-  // console.log(33333333);
   selectScrumTasks(req.params.scrumId)
-    .then(result => console.log(result.rwos[0]))
-    .catch(err => console.log(err));
-  res.send('single Tasks get');
+    .then(result => res.send(result.rows))
+    .catch(err => res.send({ errMsg: err }));
 };
+
+// const connect = require('../../database/config/connection');
+
+// const selectScrum = () => {
+//   const getScrums = {
+//     text: 'SELECT * from scrums inner join tasks on tasks.scrum_id = scrums.id',
+//   };
+//   return connect.query(getScrums);
+// };
+// exports.getTasks = (req, res) => {
+//   // console.log(222, req.params.scrumId);
+//   // console.log(33333333);
+//   selectScrum(req.params.scrumId)
+//     // .then(result => console.log(888, result.rows))
+//     .then(result => res.send(result.rows))
+//     // .catch(err => console.log(err));
+//     .catch(err => res.send({ errMsg: err }));
+// };
