cd07d0c8447a629cc4109fb1bd331ba3280f90d8
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-26 17:26:02 +0300

    modify deleteProject controller file relates #60

--- server/controllers/projects/deleteProject.js
+++ server/controllers/projects/deleteProject.js
@@ -1,1 +1,20 @@
-exports.deleteProject = (req, res) => res.send('single project delete')
+const deleteProjectQuery = require('../../database/queries/deleteProject');
+
+exports.deleteProject = (req, res, next) => {
+  const { projectId } = req.params;
+  deleteProjectQuery(projectId)
+    .then((result) => {
+      let data = null;
+      if (result.rowCount) {
+        data = true;
+      } else {
+        data = false;
+      }
+      const projectsData = {
+        data,
+        error: null,
+      };
+      res.send(projectsData);
+    })
+    .catch(e => next(e));
+};
