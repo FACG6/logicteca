94b33eeea38ccd4a3bece127b50b15ce2c6836d8
Israa <israa96sulaiman@gmail.com>
2019-04-26 23:48:03 +0300

    handle post request to user route Relates #50

--- server/controllers/users/post.js
+++ server/controllers/users/post.js
@@ -1,1 +1,23 @@
-exports.post = (req, res) => res.send('single user new');
+const insertUser = require('../../database/queries/insertUser');
+const hashedPassword = require('./helpers/hashPassword');
+const checkUser = require('../../database/queries/checkUser');
+
+exports.post = (req, res, next) => {
+  hashedPassword(req.body.password)
+    .then((password) => {
+      req.body.password = password;
+      return req.body;
+    })
+    .then(user => checkUser(user.user_name))
+    .then((results) => {
+      if (results.rowCount) {
+        return res.send({ error: { code: 401, msg: 'UserName is already taken' } });
+      } return true;
+    })
+    .then(() => insertUser(req.body))
+    .then((response) => {
+      if (response.rowCount) return res.send({ error: null, data: response.rows[0] });
+      return next({ code: 401 });
+    })
+    .catch(error => next(error));
+};
