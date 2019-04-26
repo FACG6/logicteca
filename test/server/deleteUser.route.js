480aff838a9f659c0b8f69293141766cac66fbe2
Israa <israa96sulaiman@gmail.com>
2019-04-26 16:33:34 +0300

    refactor delete

--- test/server/deleteUser.route.js
+++ test/server/deleteUser.route.js
@@ -1,16 +1,22 @@
 const test = require('tape');
 const supertest = require('supertest');
 const app = require('../../server/app');
+const selectId = require('../../server/database/queries/selectId');
 
 test('testing /users/:userId', (t) => {
-  const id = '03e2e330-141a-4bf5-baaf-bb2b6390718c';
-  supertest(app)
-    .delete(`/api/v1/users/${id}`)
-    .expect(200)
-    .expect('content-type', /json/)
-    .end((err, response) => {
-      if (err) t.error(err);
-      t.equal(response.body.data, id, 'should be equal');
+  selectId('amin')
+    .then((results) => {
+      supertest(app)
+        .delete(`/api/v1/users/${results.rows[0].id}`)
+        .expect(200)
+        .expect('content-type', /json/)
+        .end((err, response) => {
+          if (err) t.error(err);
+          t.equal(response.body.data, results.rows[0].id, 'should be equal');
+        });
+      t.end();
+    }).catch((err) => {
+      t.error(err);
+      t.end();
     });
-  t.end();
 });
