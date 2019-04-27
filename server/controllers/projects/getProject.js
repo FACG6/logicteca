940760530c6b4afc1c5db03ee3a95128594c5f03
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-27 13:04:00 +0300

    modify getProject controller file realtes #58

--- server/controllers/projects/getProject.js
+++ server/controllers/projects/getProject.js
@@ -1,14 +1,22 @@
 const selectProjectDetails = require('../../database/queries/selectProjectDetails');
 
+const refactorData = (data) => {
+  const userId = [];
+  data.forEach(element => userId.push(element.user_id));
+  const newData = data[0];
+  newData.user_id = userId;
+  return newData;
+};
+
 exports.getProject = (req, res, next) => {
   const { projectId } = req.params;
   selectProjectDetails(projectId)
     .then((result) => {
       const projectsData = {
-        data: result.rows,
+        data: refactorData(result.rows),
         error: null,
       };
-      return res.send(projectsData);
+      res.send(projectsData);
     })
     .catch(e => next(e));
 };
