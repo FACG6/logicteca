fa31fd46ecdeff6317425d1f1e9f38dbc8417bb5
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-27 13:05:49 +0300

    create getProjectRoute test file relates #58

---
+++ test/server/routes/getProjectRoute.js
@@ -0,0 +1,35 @@
+const request = require('supertest');
+const test = require('tape');
+const app = require('../../../server/app');
+const connection = require('../../../server/database/config/connection');
+
+connection
+  .query('select * from projects limit 1')
+  .then((result) => {
+    const projectId = result.rows[0].id;
+    test('Request projects route', (t) => {
+      request(app)
+        .get(`/api/v1/projects/${projectId}`)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) {
+            t.error(err);
+          } else {
+            t.deepEqual(
+              Object.keys(res.body),
+              ['data', 'error'],
+              'should return object of two main keys',
+            );
+            t.deepEqual(
+              Object.keys(res.body.data),
+              ['id', 'name', 'description', 'created_at', 'user_id'],
+              'should return all data keys',
+            );
+            t.equal(res.body.error, null, 'should return null in error key');
+            t.end();
+          }
+        });
+    });
+  })
+  .catch(e => console.log(e));
