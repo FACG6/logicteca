6a1aba81c0723fc42a6e7127f6cf628764bae51f
Israa <israa96sulaiman@gmail.com>
2019-04-27 18:46:13 +0300

    rebuild database and insertFakeData in test Relates #52

--- test/server/deleteUser.js
+++ test/server/deleteUser.js
@@ -1,10 +1,14 @@
 const test = require('tape');
 const deleteUser = require('../../server/database/queries/deleteUser');
 const selectId = require('../../server/database/queries/selectId');
+const runBuild = require('../../server/database/config/dbBuild');
+const insertFakeData = require('../../server/database/config/insertFakeData');
 
 test('test deleteUser Query', (t) => {
   const keys = ['id'];
-  selectId()
+  runBuild()
+    .then(insertFakeData)
+    .then(selectId)
     .then(response => deleteUser(response.rows[0].id))
     .then((result) => {
       t.equal(result.rowCount, 1, 'should delete one object');
--- test/server/routes/deleteUser.route.js
+++ test/server/routes/deleteUser.route.js
@@ -2,20 +2,26 @@ const test = require('tape');
 const supertest = require('supertest');
 const app = require('../../../server/app');
 const selectId = require('../../../server/database/queries/selectId');
+const runBuild = require('../../../server/database/config/dbBuild');
+const insertFakeData = require('../../../server/database/config/insertFakeData');
 
-test('testing /users/:userId', (t) => {
-  selectId()
-    .then((results) => {
+
+test('testing /users/:userId, delete route', (t) => {
+  runBuild()
+    .then(() => insertFakeData())
+    .then(() => selectId())
+    .then((result) => {
       supertest(app)
-        .delete(`/api/v1/users/${results.rows[0].id}`)
+        .delete(`/api/v1/users/${result.rows[0].id}`)
         .expect(200)
         .expect('content-type', /json/)
         .end((err, response) => {
           if (err) t.error(err);
-          t.equal(response.body.data, results.rows[0].id, 'should be equal');
+          t.equal(response.body.data, result.rows[0].id, 'should be equal');
           t.end();
         });
-    }).catch((err) => {
+    })
+    .catch((err) => {
       t.error(err);
       t.end();
     });
