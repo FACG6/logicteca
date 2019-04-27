eaa9a9ae1a0dd2c42c5eb670ed1a9d0031d56a69
Israa <israa96sulaiman@gmail.com>
2019-04-27 09:50:29 +0300

    add catch in get user controller Relates #50;

--- server/controllers/users/get.js
+++ server/controllers/users/get.js
@@ -1,9 +1,9 @@
 const selectUsers = require('../../database/queries/selectUsers');
 
-exports.get = (req, res) => {
+exports.get = (req, res, next) => {
   selectUsers()
     .then(response => response.rows)
     .then((users) => {
       res.send({ error: null, data: users });
-    });
+    }).catch(error => next(error));
 };
