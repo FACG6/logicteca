a4928dbdc44f0356056688988e896346ced1c185
Israa <israa96sulaiman@gmail.com>
2019-04-27 13:44:53 +0300

    edit deleteUser test Relates #52

--- test/server/deleteUser.js
+++ test/server/deleteUser.js
@@ -4,9 +4,10 @@ const selectId = require('../../server/database/queries/selectId');
 
 test('test deleteUser Query', (t) => {
   const keys = ['id'];
-  selectId('amin')
+  selectId()
     .then(response => deleteUser(response.rows[0].id))
     .then((result) => {
+      console.log(result);
       t.equal(result.rowCount, 1, 'should delete one object');
       t.deepEqual(Object.keys(result.rows[0]), keys, 'should contain id');
       t.end();
