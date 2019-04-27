ba3cd2978442681659b33b574d55864227d24d0d
Angham116 <angham.1161995@hotmail.com>
2019-04-27 11:44:46 +0300

    testing for api/v1/scrums/:scrumId route #55

--- test/server/getScrumRoute.js
+++ test/server/getScrumRoute.js
@@ -11,12 +11,12 @@ const selectScrum = () => {
 };
 
 test('Testing for api/v1/scrums/:scrumId route', (t) => {
-  const columns = ['id', 'name', 'project_id', 'action_type', 'status', 'modules', 'description', 'estimated_time', 'spent_time', 'priority', 'initial_test_status', 'ticket', 'notes', 'total_efforts', 'date_to_commit', 'review_and_test_note', 'scrum_id', 'assigned_to'];
+  const columns =  [ 'id', 'action_type', 'status', 'modules', 'description', 'estimated_time', 'spent_time', 'priority', 'initial_test_status', 'ticket', 'notes', 'total_efforts', 'date_to_commit', 'review_and_test_note', 'scrum_id', 'assigned_to', 'user_name', 'role'];
   selectScrum()
     .then((result) => {
       const { id } = result.rows[0];
       supertest(route)
-        .get(`api/v1/scrums/${id}`)
+        .get(`/api/v1/scrums/${id}`)
         .expect('content-type', /json/)
         .expect(200)
         .end((err, res) => {
@@ -24,7 +24,7 @@ test('Testing for api/v1/scrums/:scrumId route', (t) => {
             t.error(err);
             t.end();
           } else {
-            t.deepEqual(Object.keys(res.body.rows[0]), columns, 'Should return the same content of columns');
+            t.deepEqual(Object.keys(res.body.data[0]), columns, 'Should return the same content of columns');
             t.end();
           }
         });
