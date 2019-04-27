0325de2c01706f1c91c216c0509b85bfcca2b91a
Angham116 <angham.1161995@hotmail.com>
2019-04-27 10:30:49 +0300

    delete unnecessary  comments from file #134

--- server/controllers/scrums/getTasks.js
+++ server/controllers/scrums/getTasks.js
@@ -1,25 +1,7 @@
 const selectScrumTasks = require('../../database/queries/selectScrumTasks');
 
-exports.getTasks = (req, res) => {
+exports.getTasks = (req, res, next) => {
   selectScrumTasks(req.params.scrumId)
-    .then(result => res.send(result.rows))
-    .catch(err => res.send({ msg: 'Internal Server Error', statusCode: 500 }));
+    .then(result => res.send({ data: result.rows, error: null }))
+    .catch(err => next(err));
 };
-
-// const connect = require('../../database/config/connection');
-
-// const selectScrum = () => {
-//   const getScrums = {
-//     text: 'SELECT * from scrums inner join tasks on tasks.scrum_id = scrums.id',
-//   };
-//   return connect.query(getScrums);
-// };
-// exports.getTasks = (req, res) => {
-//   // console.log(222, req.params.scrumId);
-//   // console.log(33333333);
-//   selectScrum(req.params.scrumId)
-//     // .then(result => console.log(888, result.rows))
-//     .then(result => res.send(result.rows))
-//     // .catch(err => console.log(err));
-//     .catch(err => res.send({ errMsg: err }));
-// };
