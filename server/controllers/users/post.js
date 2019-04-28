7623ae0ea67b273b89755d006c030eb8d03644f2
Israa <israa96sulaiman@gmail.com>
2019-04-28 22:32:15 +0300

    reset post.js

--- server/controllers/users/post.js
+++ server/controllers/users/post.js
@@ -3,22 +3,21 @@ const hashedPassword = require('./helpers/hashPassword');
 const checkUser = require('../../database/queries/checkUser');
 
 exports.post = (req, res, next) => {
-  checkUser(req.body.user_name)
+  hashedPassword(req.body.password)
+    .then((password) => {
+      req.body.password = password;
+      return req.body;
+    })
+    .then(user => checkUser(user.user_name))
     .then((results) => {
       if (results.rowCount) {
         return res.status(422).send({ error: { code: 422, msg: 'UserName is already taken' } });
-      }
-      hashedPassword(req.body.password)
-        .then((password) => {
-          req.body.password = password;
-          return req.body;
-        })
-        .then(insertUser)
-        .then((response) => {
-          if (response.rowCount) return res.send({ error: null, data: response.rows[0] });
-          return next({ code: 422 });
-        })
-        .catch(error => next(error));
+      } return true;
+    })
+    .then(() => insertUser(req.body))
+    .then((response) => {
+      if (response.rowCount) return res.send({ error: null, data: response.rows[0] });
+      return next({ code: 422 });
     })
     .catch(error => next(error));
 };
