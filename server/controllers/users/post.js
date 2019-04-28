c4907b080d56e0005945f42c86dca45104b7be4b
Israa <israa96sulaiman@gmail.com>
2019-04-28 22:35:19 +0300

    checkuser before hashing password Relates #165

--- server/controllers/users/post.js
+++ server/controllers/users/post.js
@@ -3,21 +3,22 @@ const hashedPassword = require('./helpers/hashPassword');
 const checkUser = require('../../database/queries/checkUser');
 
 exports.post = (req, res, next) => {
-  hashedPassword(req.body.password)
-    .then((password) => {
-      req.body.password = password;
-      return req.body;
-    })
-    .then(user => checkUser(user.user_name))
+  checkUser(req.body.user_name)
     .then((results) => {
       if (results.rowCount) {
         return res.status(422).send({ error: { code: 422, msg: 'UserName is already taken' } });
-      } return true;
-    })
-    .then(() => insertUser(req.body))
-    .then((response) => {
-      if (response.rowCount) return res.send({ error: null, data: response.rows[0] });
-      return next({ code: 422 });
+      }
+      hashedPassword(req.body.password)
+        .then((password) => {
+          req.body.password = password;
+          return req.body;
+        })
+        .then(insertUser)
+        .then((response) => {
+          if (response.rowCount) return res.send({ error: null, data: response.rows[0] });
+          return next({ code: 422 });
+        })
+        .catch(error => next(error));
     })
     .catch(error => next(error));
 };
