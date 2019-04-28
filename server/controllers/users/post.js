f703a27e894de4851f11474c65944e83c8462c26
Israa <israa96sulaiman@gmail.com>
2019-04-28 17:32:26 +0300

    fix bug in validation Relates #158

--- server/controllers/users/post.js
+++ server/controllers/users/post.js
@@ -3,6 +3,7 @@ const hashedPassword = require('./helpers/hashPassword');
 const checkUser = require('../../database/queries/checkUser');
 
 exports.post = (req, res, next) => {
+
   hashedPassword(req.body.password)
     .then((password) => {
       req.body.password = password;
@@ -12,12 +13,13 @@ exports.post = (req, res, next) => {
     .then((results) => {
       if (results.rowCount) {
         return res.status(422).send({ error: { code: 422, msg: 'UserName is already taken' } });
-      } return true;
-    })
-    .then(() => insertUser(req.body))
-    .then((response) => {
-      if (response.rowCount) return res.send({ error: null, data: response.rows[0] });
-      return next({ code: 422 });
+      }
+      insertUser(req.body)
+        .then((response) => {
+          if (response.rowCount) return res.send({ error: null, data: response.rows[0] });
+          return next({ code: 422 });
+        })
+        .catch(error => next(error));
     })
     .catch(error => next(error));
 };
