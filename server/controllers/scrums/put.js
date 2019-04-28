7c00442a9d5f5e13322b4a49ced83d4be50726cd
aminalakhsham <aminking140@gmail.com>
2019-04-28 20:26:33 +0300

    update scrum ,query and test

--- server/controllers/scrums/put.js
+++ server/controllers/scrums/put.js
@@ -1,1 +1,26 @@
-exports.put = (req, res) => res.send('single scrum put');
+const joi = require('joi');
+
+const updateScrum = require('./../../database/queries/updateScrum');
+
+exports.put = (request, response, next) => {
+  const schema = joi.object().keys({
+    name: joi.string(),
+  });
+  const { name } = request.body;
+  const result = joi.validate({ name }, schema);
+  const { scrumId } = request.params;
+
+  if (!result.error) {
+    updateScrum(name, scrumId)
+      .then((res) => {
+        console.log(res);
+        response.send({ error: null, data: res.rows[0] });
+      })
+      .catch((error) => {
+        console.log(error);
+        next(error);
+      });
+  } else {
+    next({ code: 422 });
+  }
+};
---
+++ test/server/routes/putScrumRoute.test.js
@@ -0,0 +1,46 @@
+const test = require('tape');
+const request = require('supertest');
+const connect = require('../../../server/database/config/connection');
+const app = require('../../../server/app');
+// query for dynamic test
+const selectOneUser = () => connect.query('select id from scrums LIMIT 1');
+
+test('put in /api/v1/scrums/:scrumId (with valid data)', (t) => {
+  selectOneUser()
+    .then((res) => {
+      if (res.rowCount !== 0) {
+        console.log(res.rows[0].id.toString());
+        return res.rows[0].id;
+      }
+      t.equal(res.rowCount === 0, true, 'there is no project in data base');
+      t.end();
+      return false;
+    })
+    .then((scrumId) => {
+      request(app)
+        .put(`/api/v1/scrums/${scrumId}`)
+        .send({
+          name: 'scrum 5',
+        })
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) {
+            t.error(err);
+          } else if (res.text.error) {
+            t.error(res.text.error);
+          } else {
+            t.deepEqual(
+              Object.keys(res.body.data),
+              ['id', 'name', 'project_id'],
+              'add project and it users sucssfully',
+            );
+            t.end();
+          }
+        });
+    })
+    .catch((error) => {
+      t.error(error);
+    });
+});
+test.onFinish(() => process.exit(0));
