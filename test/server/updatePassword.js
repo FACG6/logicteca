7786871e978600eeecb440457e9aed6246c5080d
Israa <israa96sulaiman@gmail.com>
2019-04-28 16:39:07 +0300

    test updatePassword query and endpoint Relates #158

---
+++ test/server/updatePassword.js
@@ -0,0 +1,17 @@
+const test = require('tape');
+
+const updatePassword = require('../../server/database/queries/updatePassword');
+const selectId = require('../../server/database/queries/selectId');
+
+test('test update password query', (t) => {
+  const newPassword = '123456israa';
+  selectId()
+    .then(result => updatePassword(newPassword, result.rows[0].id))
+    .then((response) => {
+      t.deepEqual(Object.keys(response.rows[0]), ['id'], 'should contain id');
+      t.end();
+    }).catch((err) => {
+      t.error(err);
+      t.end();
+    });
+});
