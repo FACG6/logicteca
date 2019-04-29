f008fe8a0bf48961cdbf3978bcc1378a954f9347
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-29 16:17:25 +0300

    modify login form relates #167

--- client/src/components/Login/loginForm/index.jsx
+++ client/src/components/Login/loginForm/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import image from './user.png';
 import { Icon } from 'antd';
@@ -8,15 +9,21 @@ export default class Login extends Component {
   state = {
     user_name: '',
     password: '',
-    loginError: ''
+    loginError: '',
+    redirect: ''
   };
 
   handleChange = ({ target: { name, value } }) => {
     this.setState({ [name]: value.trim() });
   };
-
   login = event => {
     event.preventDefault();
+    let pathname = '';
+    console.log(this.props.location, 5555);
+
+    this.props.location.state.from
+      ? ({ pathname } = this.props.location.state.from)
+      : (pathname = '/');
     const { password, user_name } = this.state;
     const { setUserInfo } = this.props;
     if (!password || !user_name) {
@@ -30,7 +37,8 @@ export default class Login extends Component {
         })
         .then(result => {
           if (result.data) {
-            return setUserInfo(result.data);
+            setUserInfo(result.data);
+            this.setState({ redirect: pathname });
           }
         })
         .catch(e =>
@@ -43,44 +51,47 @@ export default class Login extends Component {
 
   render() {
     return (
-      <section className="login__form-container">
-        <img className="login__image" src={image} alt="user" />
-        <form onSubmit={this.login} className="login__form">
-          <div className="login__input-container">
-            <Icon className="login__icon" type="user" />
+      <>
+        {this.state.redirect ? <Redirect to={this.state.redirect} /> : null}
+        <section className="login__form-container">
+          <img className="login__image" src={image} alt="user" />
+          <form onSubmit={this.login} className="login__form">
+            <div className="login__input-container">
+              <Icon className="login__icon" type="user" />
+              <input
+                onChange={this.handleChange}
+                placeholder="Username"
+                className="login__input"
+                id="username"
+                name="user_name"
+                type="text"
+                value={this.state.username}
+              />
+            </div>
+            <div className="login__input-container">
+              <Icon className="login__icon" type="lock" />
+              <input
+                onChange={this.handleChange}
+                placeholder="Password"
+                className="login__input"
+                id="password"
+                name="password"
+                type="password"
+                value={this.state.password}
+              />
+            </div>
+            {this.state.loginError ? (
+              <span className="login__error">{this.state.loginError}</span>
+            ) : null}
             <input
-              onChange={this.handleChange}
-              placeholder="Username"
-              className="login__input"
-              id="username"
-              name="user_name"
-              type="text"
-              value={this.state.username}
+              className="login__submit-btn"
+              id="login"
+              type="submit"
+              value="Login"
             />
-          </div>
-          <div className="login__input-container">
-            <Icon className="login__icon" type="lock" />
-            <input
-              onChange={this.handleChange}
-              placeholder="Password"
-              className="login__input"
-              id="password"
-              name="password"
-              type="password"
-              value={this.state.password}
-            />
-          </div>
-          {this.state.loginError ? (
-            <span className="login__error">{this.state.loginError}</span>
-          ) : null}
-          <input
-            className="login__submit-btn"
-            id="login"
-            type="submit"
-            value="Login"
-          />
-        </form>
-      </section>
+          </form>
+        </section>
+      </>
     );
   }
 }
