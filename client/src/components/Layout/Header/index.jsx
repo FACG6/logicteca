90bc758ee3975e9049aa6d88fc0e9d24db0b7044
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-29 14:22:24 +0300

    modify header with logged in user info relates #167

--- client/src/components/Layout/Header/index.jsx
+++ client/src/components/Layout/Header/index.jsx
@@ -6,40 +6,63 @@ import './style.css';
 
 class Header extends Component {
   state = {
-    username: 'Mohanned',
-    role: 'admin',
-  }
+    username: '',
+    role: ''
+  };
 
   menu = (
     <Menu>
       <Menu.Item>
-        <a href='/logout'>Logout</a>
+        <a href="/logout">Logout</a>
       </Menu.Item>
     </Menu>
   );
 
+  componentDidMount() {
+    const {
+      userInfo: { user_name, role }
+    } = this.props;
+    this.setState({ username: user_name, role: role });
+  }
   render() {
     return (
-      <header className='header'>
-        <div className='header__container'>
-          <img alt='logo' className='header__logo' src='http://logicteca.com/wp-content/uploads/2015/11/logicteca-logo.png' />
-          <div className='header__right-section'>
-            <ul className='header__navbar'>
-              <NavLink to='/projects'><li> Projects </li></NavLink>
-              {this.state.role === 'admin' ? <NavLink to='/users'><li> Users </li></NavLink> : null}
+      <header className="header">
+        <div className="header__container">
+          <img
+            alt="logo"
+            className="header__logo"
+            src="http://logicteca.com/wp-content/uploads/2015/11/logicteca-logo.png"
+          />
+          <div className="header__right-section">
+            <ul className="header__navbar">
+              <NavLink to="/projects">
+                <li> Projects </li>
+              </NavLink>
+              {this.state.role === 'admin' ? (
+                <NavLink to="/users">
+                  <li> Users </li>
+                </NavLink>
+              ) : null}
             </ul>
-            <div className='header__username'>
-              <Dropdown trigger={['click']} overlay={this.menu} placement="bottomLeft">
+            <div className="header__username">
+              <Dropdown
+                trigger={['click']}
+                overlay={this.menu}
+                placement="bottomLeft"
+              >
                 <div>
                   <span>{this.state.username}</span>
-                  <FontAwesomeIcon className='header__caret-down' icon='caret-down' />
+                  <FontAwesomeIcon
+                    className="header__caret-down"
+                    icon="caret-down"
+                  />
                 </div>
               </Dropdown>
             </div>
           </div>
         </div>
       </header>
-    )
+    );
   }
 }
 
