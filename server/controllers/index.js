9b6a5a3628b3ba3ac1ff51f13409c1c8c351d0e1
aminalakhsham <aminking140@gmail.com>
2019-04-29 14:07:42 +0300

    comment middleware line in index controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,7 +5,7 @@ const scrums = require('./scrums');
 const tasks = require('./tasks');
 const { auth } = require('./middleware/authentication');
 
-router.use(auth);
+// router.use(auth);
 router.use('/projects', projects);
 router.use('/users', users);
 router.use('/scrums', scrums);
