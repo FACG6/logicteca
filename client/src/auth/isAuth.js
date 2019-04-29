ca88095bb6abaafb20be575879b70085823788c0
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-29 12:33:16 +0300

    add isAuth function relates #167

---
+++ client/src/auth/isAuth.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+export default function isAuth() {
+  //   axios
+  //     .get('/api/v1/isAuthenticated')
+  //     .then(result => {
+  //       if (result.isAuthenticated) {
+  //         setUserInfo(result.userInfo);
+  //         return true;
+  //       }
+  //       return false;
+  //     })
+  //     .catch(e => {
+  //       return false;
+  //     });
+
+  return false;
+}
