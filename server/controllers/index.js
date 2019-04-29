35b2990e5d7f8bb359ea5709e61db5bfe4860839
Angham116 <angham.1161995@hotmail.com>
2019-04-29 14:31:20 +0300

    add new route for loginPost route #170

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,7 +3,10 @@ const projects = require('./projects');
 const users = require('./users');
 const scrums = require('./scrums');
 const tasks = require('./tasks');
+const login = require('./login');
 
+
+router.use('/login', login)
 router.use('/projects', projects);
 router.use('/users', users);
 router.use('/scrums', scrums);
---
+++ server/controllers/login/login.js
@@ -0,0 +1,29 @@
+const { compare } = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const checkUser = require('../../database/queries/checkUser');
+
+exports.post = (request, response) => {
+  const { user_name, password } = request.body;
+  let payload;
+  checkUser(user_name)
+    .then((res) => {
+      if (res.rows[0]) {
+        const { user_name, role } = res.rows[0];
+        payload = {
+          user_name,
+          role,
+        };
+        return compare(password, res.rows[0].password);
+      }
+    })
+    .then((isHashed) => {
+      if (!isHashed) {
+        response.status(401).send({ msg: 'Invalid username or password' });
+      } else {
+        const token = jwt.sign(payload, process.env.SECRET);
+        response.cookie('jwt', token, { maxAge: 60 * 60 * 24 }, { httpOnly: true });
+        response.status(201).send({ msg: 'Success' });
+      }
+    })
+    .catch(() => response.status(401).send({ msg: 'Invalid username or password' }));
+};
