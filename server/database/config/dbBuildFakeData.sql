1a64b6f8c99a6458a4064d6b9f5bb9939c4d16a0
Angham116 <angham.1161995@hotmail.com>
2019-04-29 14:30:22 +0300

    add new row of user with hashed password #170

--- server/database/config/dbBuildFakeData.sql
+++ server/database/config/dbBuildFakeData.sql
@@ -3,6 +3,7 @@ BEGIN;
 INSERT INTO users (user_name,full_name,password,role) VALUES ('amin','Amin ALakhsham','123','Developer');
 INSERT INTO users (user_name,full_name,password,role) VALUES ('israa','Israa Suliman','123','Scrum');
 INSERT INTO users (user_name,full_name,password,role) VALUES ('ahmed','Ahmed Shatat','123','Developer');
+INSERT INTO users (user_name,full_name,password,role) VALUES ('Angham116','Angham116','$2a$10$ELrdejLuU6uuCfHilqwr8OJ4JxqbEoVMyckfQjfjCg5K8j8qaka7y','Developer');
 
 INSERT INTO projects (name,description) VALUES ('logicteca','Custom built task management system');
 INSERT INTO projects (name,description) VALUES ('AMIDEAST','connecting students with our different programs has been a challenge');
