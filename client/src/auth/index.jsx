0e44898f50e1a172bfb620162ec49170f2f3f505
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-29 12:32:31 +0300

    add private route relates #167

--- client/src/auth/index.jsx
+++ client/src/auth/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import isAuth from './isAuth';
+import { Route, Redirect } from 'react-router-dom';
+import { checkPropTypes } from 'prop-types';
 
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        return isAuth() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: { from: props.location } }}
+          />
+        );
+      }}
+    />
+  );
+};
 
-const PrivateRoute = ({component: Component, ...rest}) => {
-    return ( 
-        
-     );
-}
- 
 export default PrivateRoute;
