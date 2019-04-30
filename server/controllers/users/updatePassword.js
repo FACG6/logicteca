945565b755a970ac41781a540e1a0006c25d0243
Israa <israa96sulaiman@gmail.com>
2019-04-30 10:23:19 +0300

    change the name of query

--- server/controllers/users/updatePassword.js
+++ server/controllers/users/updatePassword.js
@@ -1,7 +1,7 @@
 const Joi = require('@hapi/joi');
 
 const hashPassword = require('./helpers/hashPassword');
-const updatePassword = require('../../database/queries/updatePassword');
+const updatePasswordQuery = require('../../database/queries/updatePassword');
 
 const schema = Joi.object().keys({
   password: Joi.string().min(6),
@@ -12,7 +12,7 @@ exports.postPassword = (req, res, next) => {
   Joi.validate(req.body, schema)
     .then((password) => {
       hashPassword(password.password)
-        .then(hashedPassword => updatePassword(hashedPassword, userId))
+        .then(hashedPassword => updatePasswordQuery(hashedPassword, userId))
         .then((response) => {
           if (response.rowCount) res.send({ error: null, data: 'success' });
           else next({ code: 400 });
