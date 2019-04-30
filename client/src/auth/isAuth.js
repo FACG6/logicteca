d78d72956452b9202fbdbd33861eb3bc89206882
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-30 14:19:24 +0300

    check result.data relates #184

--- client/src/auth/isAuth.js
+++ client/src/auth/isAuth.js
@@ -1,11 +1,13 @@
 const axios = require('axios');
 
 export default function isAuth() {
-  // axios
-  //   .get('/api/v1/isAuthenticated')
-  //   .then(result => (result.data.isAuthenticated ? true : false))
-  //   .catch(e => {
-  //     return false;
-  //   });
-  return true;
+  axios
+    .get('/api/v1/isAuthenticated')
+    .then(result => {
+      return result.data ? true : false;
+    })
+    .catch(e => {
+      console.log(e, 'false');
+      return false;
+    });
 }
