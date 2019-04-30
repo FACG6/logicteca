f474bf51246852dd63f37e88988d51c96f8cec03
AymanAlQoqa <aalqouqa@gmail.com>
2019-04-30 19:23:47 +0300

    remove console.log relates #184

--- client/src/auth/isAuth.js
+++ client/src/auth/isAuth.js
@@ -3,11 +3,6 @@ const axios = require('axios');
 export default function isAuth() {
   axios
     .get('/api/v1/isAuthenticated')
-    .then(result => {
-      return result.data ? true : false;
-    })
-    .catch(e => {
-      console.log(e, 'false');
-      return false;
-    });
+    .then(result => (result.data ? true : false))
+    .catch(e => false);
 }
