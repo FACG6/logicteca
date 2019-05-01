4c3de2125529dfad6ea88f6aa08826c77b4b3c09
aminalakhsham <aminking140@gmail.com>
2019-05-01 16:09:47 +0300

    handle request for delete task Relates #160

--- server/controllers/tasks/deleteTask.js
+++ server/controllers/tasks/deleteTask.js
@@ -1,1 +1,10 @@
-exports.deleteTask = (req, res) => res.send('single task delete');
+const deleteTask = require('./../../database/queries/deleteTask');
+
+exports.deleteTask = (request, response, next) => {
+  const { scrumId } = request.params;
+  deleteTask(scrumId)
+    .then((res) => {
+      response.send({ error: null, data: res.rows[0] });
+    })
+    .catch(error => next(error));
+};
