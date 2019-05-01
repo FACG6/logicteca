a4437e1f9d1637257484b5c9c59d0b4dd76437c2
AymanAlQoqa <aalqouqa@gmail.com>
2019-05-01 16:14:09 +0300

    order the returned data by DESC relates #195

--- server/database/queries/selectProjects.js
+++ server/database/queries/selectProjects.js
@@ -1,8 +1,5 @@
 const connect = require('./../config/connection');
 
-const selectAllProject = () => connect.query(`SELECT 
-                                                    * 
-                                              FROM 
-                                                       projects`);
+const selectAllProject = () => connect.query('SELECT * FROM projects ORDER BY id DESC');
 
 module.exports = selectAllProject;
