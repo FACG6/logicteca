227b9b499d032bdee34a90eab3b623fb60e61a20
Angham116 <angham.1161995@hotmail.com>
2019-05-01 15:55:27 +0300

    route for update task

--- server/controllers/tasks/put.js
+++ server/controllers/tasks/put.js
@@ -1,1 +1,10 @@
-exports.put = (req, res) => res.send('single task put');
+const updateTask = require('../../database/queries/updateTask');
+
+exports.put = (req, res, next) => {
+  console.log(111, req.body);
+  updateTask(req.body)
+    .then(() => {
+      res.send({ error: null, data: req.body });
+    })
+    .catch(error => next(error));
+};
