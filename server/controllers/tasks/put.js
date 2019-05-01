b3308d8f47950983496f02cf1b52ead257afa4a4
Angham116 <angham.1161995@hotmail.com>
2019-05-01 16:00:24 +0300

    delete comment

--- server/controllers/tasks/put.js
+++ server/controllers/tasks/put.js
@@ -1,7 +1,6 @@
 const updateTask = require('../../database/queries/updateTask');
 
 exports.put = (req, res, next) => {
-  console.log(111, req.body);
   updateTask(req.body)
     .then(() => {
       res.send({ error: null, data: req.body });
