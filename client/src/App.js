92ce619f1d265db0b80f3b423d9b81d79d7b6424
AymanAlQoqa <aalqouqa@gmail.com>
2019-05-02 10:04:03 +0300

    add check in componentDidMount relates #203

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import 'antd/dist/antd.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import isAuth from './auth/isAuth';
+
 import {
   faTrash,
   faFilter,
@@ -35,8 +37,13 @@ library.add(
 class App extends Component {
   state = {
     userInfo: {},
-    isLogin: true,
+    isLogin: false
   };
+  componentDidMount() {
+    isAuth
+      ? this.setState({ isLogin: true })
+      : this.setState({ isLogin: false });
+  }
   setUserInfo = user => {
     this.setState({ userInfo: user, isLogin: true });
   };
