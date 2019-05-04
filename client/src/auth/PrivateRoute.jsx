df6674b369567c913ac8f8d2e14a72c56ff3065b
Ahmed Shatat <a.shatat@hotmail.com>
2019-05-04 10:35:28 +0300

    refactor privateRoute.jsx #216

--- client/src/auth/PrivateRoute.jsx
+++ client/src/auth/PrivateRoute.jsx
@@ -1,25 +1,66 @@
-import React from "react";
-import isAuth from "./isAuth";
-import { Route, Redirect } from "react-router-dom";
+import React, { Component } from "react";
+import { Route, withRouter } from "react-router-dom";
+import axios from 'axios';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={props => {
-        isAuth(state => {
-          if (!state) {
-            rest.clearUserInfo();
+class PrivateRoute extends Component {
+  // axios configuration
+  axiosSource = axios.CancelToken.source();
+  axiosInstance = axios.create({
+    baseURL: '/api/v1/',
+    cancelToken: this.axiosSource.token
+  });
+
+  state = {
+    isAuth: false,
+  }
+
+  componentWillUnmount() {
+    this.axiosSource.cancel('cancel isAuthenticated request');
+  }
+
+  componentDidMount() {
+    this.axiosInstance
+    .get('/isAuthenticated')
+      .then(() => {
+        this.setState({ isAuth: true });
+      })
+      .catch(() => {
+        this.isUnAuthorized();
+      });
+  }
+
+  componentDidUpdate(prevProps) {
+    // this will stop when componentDidMount works
+    if (this.props.location.key !== prevProps.location.key) {
+      this.axiosInstance
+      .get('/isAuthenticated')
+        .then(() => {
+          if (!this.state.isAuth) {
+            this.setState({ isAuth: true });
           }
+        })
+        .catch(() => {
+          this.isUnAuthorized();
         });
-        return (
-          <>
-            <Component {...props} />
-          </>
-        );
-      }}
-    />
-  );
+    }
+  }
+  
+  isUnAuthorized = () => {
+    this.props.history.push('/login');
+  }
+
+  render() {
+    const { component: Component, ...rest } = this.props;
+    const { isAuth } = this.state;
+
+    return (
+      isAuth &&
+      <Route
+        {...rest}
+        render={props => <Component {...props} />}
+        />
+    );
+  }
 };
 
-export default PrivateRoute;
+export default withRouter(PrivateRoute);
