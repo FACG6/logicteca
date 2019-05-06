5e8fd0390410c914b2cccf3ffdd6417f4bbc4356
Israa <israa96sulaiman@gmail.com>
2019-05-06 22:54:58 +0300

    edit a bug in getScrums route Relates #247

--- server/controllers/projects/getScrums.js
+++ server/controllers/projects/getScrums.js
@@ -1,15 +1,17 @@
 const selectProjectScrum = require('../../database/queries/selectProjectScrums');
 const selectProjectDetails = require('../../database/queries/selectProjectDetails');
+const refactorData = require('./helper');
 
 exports.getScrums = (req, res, next) => {
   const { projectId } = req.params;
   selectProjectDetails(projectId)
-    .then(result => result.rows[0])
+    .then(result => result.rows)
     .then((details) => {
+      const project = refactorData(details);
       selectProjectScrum(projectId)
         .then((scrums) => {
           res.send({
-            data: { scrums: scrums.rows, project: details },
+            data: { scrums: scrums.rows, project },
             error: null,
           });
         })
